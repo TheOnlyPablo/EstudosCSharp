f880fffabb363c852b1681bf81be45e054ba2ff7
Pablo Almeida de Souza <pabloas999@gmail.com>
2023-06-12 05:40:43 -0300

    Exercicio Proposto de Herança e Polimorfismo

---
+++ ExerHeraPoli/ExerHeraPoli/Entities/UsedProcut.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ExerHeraPoli.Entities
+{
+    class UsedProcut : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+        public UsedProcut() { }
+
+        public UsedProcut(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+
+    }
+}
